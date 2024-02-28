4be91bb398b3f42459136b98da89c210951786f8
Ian-Stolte <ianstolte@gmail.com>
2024-02-28 15:13:32 -0800

    Syncing asteroids to client

--- Stranded/Assets/Scripts/Spawner.cs
+++ Stranded/Assets/Scripts/Spawner.cs
@@ -1,26 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Netcode;
 
-public class Spawner : MonoBehaviour
+public class AsteroidSpawner : NetworkBehaviour
 {
     [SerializeField] GameObject prefab;
     [SerializeField] float minDelay;
     [SerializeField] float maxDelay;
     [SerializeField] float minDistance;
     [SerializeField] float maxDistance;
+    [SerializeField] float minSpeed;
+    [SerializeField] float maxSpeed;
+    [SerializeField] float minSize;
+    [SerializeField] float maxSize;
     float timer;
 
     void Update()
     {
-        timer -= Time.deltaTime;
-
-        if (timer <= 0)
+        if (IsServer)
         {
-            timer = Random.Range(minDelay, maxDelay);
-            Vector3 distance = new Vector3(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f), 0);
-            distance = Vector3.Normalize(distance) * Random.Range(minDistance, maxDistance);
-            Instantiate(prefab, transform.position+distance, transform.rotation, GameObject.Find("Asteroids").transform);
+            timer -= Time.deltaTime;
+
+            if (timer <= 0)
+            {
+                timer = Random.Range(minDelay, maxDelay);
+                Vector3 distance = new Vector3(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f), 0);
+                distance = Vector3.Normalize(distance) * Random.Range(minDistance, maxDistance);
+                GameObject obj = Instantiate(prefab, transform.position + distance, transform.rotation, GameObject.Find("Asteroids").transform);
+                //Set asteroid values
+                AsteroidBehavior ast = obj.GetComponent<AsteroidBehavior>();
+                ast.speed = Random.Range(minSpeed, maxSpeed);
+                obj.transform.localScale = new Vector3(Random.Range(minSize, maxSize), Random.Range(minSize, maxSize), 1);
+                var euler = transform.eulerAngles;
+                euler.z = Random.Range(0, 360);
+                obj.transform.eulerAngles = euler;
+                ast.direction = new Vector3(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f), 0);
+                ast.direction = Vector3.Normalize(ast.direction);
+                obj.GetComponent<NetworkObject>().Spawn();
+            }
         }
     }
 }
--- Stranded_clone_1/Assets/Scripts/AsteroidBehavior.cs
+++ Stranded_clone_1/Assets/Scripts/AsteroidBehavior.cs
@@ -4,29 +4,14 @@ using UnityEngine;
 
 public class AsteroidBehavior : MonoBehaviour
 {
-    [Header("Spawn Parameters")]
-    [SerializeField] float minSpeed;
-    [SerializeField] float maxSpeed;
-    [SerializeField] float minSize;
-    [SerializeField] float maxSize;
+    public float speed;
+    public Vector3 direction;
     [SerializeField] float despawnDistance;
-
-    [Header("Movement")]
-    [SerializeField] float speed;
-    [SerializeField] Vector3 direction;
 
     GameObject ship;
 
     void Start()
     {
-        speed = Random.Range(minSpeed, maxSpeed);
-        transform.localScale = new Vector3(Random.Range(minSize, maxSize), Random.Range(minSize, maxSize), 1);
-    	var euler = transform.eulerAngles;
-	    euler.z = Random.Range(0, 360);
-	    transform.eulerAngles = euler;
-        direction = new Vector3(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f), 0);
-        direction = Vector3.Normalize(direction);
-
         ship = GameObject.Find("Spaceship");
     }
 
