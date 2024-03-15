0f2b76ffcd652a953269e675243ab8bc2644b66d
lk3nn3y <luciak24@lakesideschool.org>
2024-03-15 14:40:47 -0700

    Shield Player Station
    Added shield player station button. In the station you can rotate the shield using a and d keys.

--- Stranded/Assets/Scripts/Shield.cs
+++ Stranded/Assets/Scripts/Shield.cs
@@ -5,29 +5,16 @@ using Unity.Netcode;
 
 public class Shield : MonoBehaviour
 {
-    public GameObject anchor;
-    public int velocity;
 
     // Start is called before the first frame update
     void Start()
     {
-        velocity = 20;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        
-        if (Input.GetKey(KeyCode.Z))
-        {
-            transform.RotateAround(anchor.transform.localPosition, Vector3.forward, velocity);
-        }
 
-        if (Input.GetKey(KeyCode.C))
-        {
-            transform.RotateAround(anchor.transform.localPosition, Vector3.back, velocity);
-        }
     }
 
     void OnTriggerEnter2D(Collider2D collider)
