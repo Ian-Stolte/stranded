b85dfa655d7626c81bb0b1e118751bf936a0e265
Emily Peng <awesome123emily@gmail.com>
2024-05-02 22:48:11 -0700

    "Next level" info generalized
    Can change for specific stations later

--- Stranded/Assets/Scripts/StationTemplate.cs
+++ Stranded/Assets/Scripts/StationTemplate.cs
@@ -6,6 +6,6 @@ using TMPro;
 public class StationTemplate : MonoBehaviour
 {
    public TMP_Text stationLevelText;
-    public TMP_Text stationName;
+    public TMP_Text nextLevelInfo;
     public int stationLevel;
 }
