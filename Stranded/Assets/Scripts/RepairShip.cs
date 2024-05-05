4e21ddfd7f4a6b1e2efb084cd09f17d9b63a222d
Emily Peng <awesome123emily@gmail.com>
2024-05-04 22:30:19 -0700

    Add "repair ship" item

---
+++ Stranded/Assets/Scripts/RepairShip.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(menuName = "Boosts/RepairShip")]
+public class RepairShip : BoostEffect
+{
+    public int amount;
+
+    public override void Apply(GameObject target)
+    {
+        // Update health variable
+        target.GetComponent<Spaceship>().shipHealth.Value = Mathf.Min(target.GetComponent<Spaceship>().shipHealth.Value+amount, target.GetComponent<Spaceship>().shipHealthMax);
+        
+        // Update health bar
+        GameObject.Find("Ship Damage Bar").GetComponent<Image>().fillAmount = target.GetComponent<Spaceship>().shipHealth.Value/target.GetComponent<Spaceship>().shipHealthMax;
+        GameObject.Find("Sync Object").GetComponent<Sync>().ChangeHealthClientRpc(target.GetComponent<Spaceship>().shipHealth.Value, target.GetComponent<Spaceship>().shipHealthMax);
+    }
+}
