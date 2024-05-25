076541dc2aae0ad18be3a496c2cf9b626610706f
lk3nn3y <luciak24@lakesideschool.org>
2024-05-25 13:32:55 -0700

    Updated Radio Part Collection
    Added scaling of percent chance of collecting radio part depending on difficulty. Corrected an error in the if statement--now the code checks if radar has been unlocked as a criteria for getting a radio part

--- Stranded/Assets/Scripts/Spaceship.cs
+++ Stranded/Assets/Scripts/Spaceship.cs
@@ -51,8 +51,8 @@ public class Spaceship : NetworkBehaviour
     public NetworkVariable<float> radioParts;
     public NetworkVariable<float> fuelAmount;
     public float fuelMax;
-    [Tooltip("How many seconds between each fuel depletion")] [SerializeField] private float depletionInterval;
-    [Tooltip("How much fuel depletes each interval")] [SerializeField] private float depletionAmount;
+    [Tooltip("How many seconds between each fuel depletion")][SerializeField] private float depletionInterval;
+    [Tooltip("How much fuel depletes each interval")][SerializeField] private float depletionAmount;
     public GameObject resourceTextPrefab;
     private float gameTime;
 
@@ -74,6 +74,7 @@ public class Spaceship : NetworkBehaviour
     [HideInInspector] public bool radarUnlocked;
     [HideInInspector] public int radarRange;
     [HideInInspector] public bool multipleArrows;
+    public float radioChance;
 
     //Grabber
     [HideInInspector] public bool grabberUnlocked;
@@ -82,22 +83,23 @@ public class Spaceship : NetworkBehaviour
     private float multiplierThreshold;
 
     //Upgrades
-    private float[] turnSpeeds = new float[] {1, 1.5f, 2, 2.5f};
-    private float[] thrustSpeeds = new float[] {2, 3, 3, 4.5f};
-    private float[] shieldSpeeds = new float[] {1.5f, 2, 2, 3};
-    private float[] maxSpeeds = new float[] {7, 9, 9, 12};
-    private int[] radarRanges = new int[] {75, 125, 125, 200};
-    private int[] grabberRanges = new int[] {12, 12, 15, 20};
-    private float[] grabberMultiplierThresholds = new float[] {1, 0.7f, 0.7f, 0.4f};
-    private float[] grabberSpeeds = new float[] {0.5f, 0.5f, 0.55f, 0.7f};
-    private float[] grabberRetractSpeeds = new float[] {0.2f, 0.2f, 0.25f, 0.35f};
+    private float[] turnSpeeds = new float[] { 1, 1.5f, 2, 2.5f };
+    private float[] thrustSpeeds = new float[] { 2, 3, 3, 4.5f };
+    private float[] shieldSpeeds = new float[] { 1.5f, 2, 2, 3 };
+    private float[] maxSpeeds = new float[] { 7, 9, 9, 12 };
+    private int[] radarRanges = new int[] { 75, 125, 125, 200 };
+    private int[] grabberRanges = new int[] { 12, 12, 15, 20 };
+    private float[] grabberMultiplierThresholds = new float[] { 1, 0.7f, 0.7f, 0.4f };
+    private float[] grabberSpeeds = new float[] { 0.5f, 0.5f, 0.55f, 0.7f };
+    private float[] grabberRetractSpeeds = new float[] { 0.2f, 0.2f, 0.25f, 0.35f };
 
     //Difficulty levels
-    private float[] dmgLevels = new float[] {0.5f, 1, 1.5f, 2};
-    private float[] depletionIntervals = new float[] {8, 6.5f, 5, 4};
-    private (float min, float max)[] asteroidSpeeds = new (float min, float max)[] {(0.5f, 2), (0.5f, 2.5f), (1, 3), (1.5f, 3.5f)};
-    private (float min, float max)[] asteroidDelays = new (float min, float max)[] {(2, 5), (1, 4), (1, 3.5f), (0.5f, 3)};
-    private (float min, float max)[] shipwreckDelays = new (float min, float max)[] {(5, 15), (10, 20), (10, 20), (15, 30)};
+    private float[] dmgLevels = new float[] { 0.5f, 1, 1.5f, 2 };
+    private float[] depletionIntervals = new float[] { 8, 6.5f, 5, 4 };
+    private (float min, float max)[] asteroidSpeeds = new (float min, float max)[] { (0.5f, 2), (0.5f, 2.5f), (1, 3), (1.5f, 3.5f) };
+    private (float min, float max)[] asteroidDelays = new (float min, float max)[] { (2, 5), (1, 4), (1, 3.5f), (0.5f, 3) };
+    private (float min, float max)[] shipwreckDelays = new (float min, float max)[] { (5, 15), (10, 20), (10, 20), (15, 30) };
+    private float[] radioChances = new float[] {0.10f, 0.05f, 0.035f, 0.02f};
     private int[] maxShipwrecks = new int[] {10, 10, 8, 8};
 
     void Start()
@@ -124,6 +126,7 @@ public class Spaceship : NetworkBehaviour
         wreckSpawner.minDelay = shipwreckDelays[difficulty].min;
         wreckSpawner.maxDelay = shipwreckDelays[difficulty].max;
         wreckSpawner.maxAtOnce = maxShipwrecks[difficulty];
+        radioChance = radioChances[difficulty];
     }
 
     void Update()
@@ -263,9 +266,10 @@ public class Spaceship : NetworkBehaviour
             }
             //radio parts chance
             float radioRandValue = Random.value;
-            if (radioRandValue < 0.05)
+            if (radarUnlocked == true && radioRandValue < radioChance) //A radio part is found
             {
                 radioParts.Value += 1 ;
+                shop.AddRadioParts();
             }
             if (player.usedRadar)
             {
@@ -285,7 +289,6 @@ public class Spaceship : NetworkBehaviour
                 ResourceTextClientRpc(collider.gameObject.GetComponent<ShipwreckBehavior>().value.Value * multiplier, true);
             }
             shop.AddScraps();
-            shop.AddRadioParts();
         }
     }
 
